66dbc46e8b0d05dad2651a83cf72355e7c0a8455
MartinMZen <mmelekus@users.noreply.github.com>
2024-08-09 21:23:43 -0500

    First commit

---
+++ MvcApp/MvcMovie/Models/Movie.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string Title { get; set; } = null!;
+
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    public DateTime ReleaseDate { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [StringLength(30)]
+    public string Genre { get; set; } = null!;
+
+    [Range(1, 100)]
+    [Column(TypeName = "decimal(18,2)")]
+    [DataType(DataType.Currency)]
+    public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    public string Rating { get; set; } = null!;
+}
